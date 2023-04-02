86e2a5c745631bbc3b124b1b802a30f9a04a3333
rm-rfvi <129233522+rm-rfvi@users.noreply.github.com>
2023-04-02 15:33:06 +0930

    add watchtower

--- docker-provisioner.sh
+++ docker-provisioner.sh
@@ -21,6 +21,37 @@ mkdir -p /opt/docker/tmp
 
 chmod 755 /opt/docker/run
 chmod 755 /opt/docker/tmp
+chmod 755 /opt/docker/build
 
+# Create the directory if it doesn't already exist
+if [ ! -d "/opt/docker/build/watchtower" ]; then
+  mkdir -p /opt/docker/build/watchtower
+fi
 
-cd /opt/docker/build
+# Create the docker-compose.yaml file
+cat > /opt/docker/build/watchtower/docker-compose.yaml <<EOF
+version: "3"
+services:
+  watchtower:
+    image: containrrr/watchtower
+    restart: always
+    volumes:
+      - /etc/localtime:/etc/localtime:ro
+      - /var/run/docker.sock:/var/run/docker.sock:ro
+      - /etc/timezone:/etc/timezone:ro
+    environment:
+      - PUID=1000
+      - PGID=1000
+      - TZ=Australia/Adelaide
+      - UMASK_SET=022 #optional
+      - WATCHTOWER_CLEANUP=true
+      - WATCHTOWER_LABEL_ENABLE=true
+      - WATCHTOWER_INCLUDE_RESTARTING=true
+    labels:
+      - "com.centurylinklabs.watchtower.enable=true"
+EOF
+
+# Run docker-compose up against the docker-compose.yaml file
+cd /opt/docker/build/watchtower
+docker-compose up -d
+

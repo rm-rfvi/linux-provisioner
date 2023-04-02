a8ea3fdf0400cd389eaa001393a32c754a479cba
rm-rfvi <129233522+rm-rfvi@users.noreply.github.com>
2023-04-02 15:26:55 +0930

    add ipv6 disable

--- core-provisioner.sh
+++ core-provisioner.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 
+echo "Disabling IPv6"
+echo "net.ipv6.conf.all.disable_ipv6 = 1" | sudo tee /etc/sysctl.d/99-disable-ipv6.conf
+echo "net.ipv6.conf.default.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.d/99-disable-ipv6.conf
+echo "net.ipv6.conf.lo.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.d/99-disable-ipv6.conf
+sudo sysctl -p
+echo "IPv6 disabled successfully"
 
 
 sudo apt update

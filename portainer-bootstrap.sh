8bb82f0a87494de1b0948241b720810db5166691
valley_one <129233522+rm-rfvi@users.noreply.github.com>
2023-04-22 22:27:41 +0930

    Update portainer-bootstrap.sh

--- portainer-bootstrap.sh
+++ portainer-bootstrap.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+TIMEZONE="Australia/Adelaide"
 
 
 echo "Disabling IPv6"
@@ -8,15 +8,6 @@ echo "net.ipv6.conf.lo.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.d/99-disable-
 sudo sysctl -p
 echo "IPv6 disabled successfully"
 
-
-sudo apt update
-sudo apt install \
-  ca-certificates \
-  curl \
-  gnupg \
-
-
-TIMEZONE="Australia/Adelaide"
 
 sudo mkdir -m 0755 -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -37,8 +28,10 @@ mkdir -p /opt/docker/run
 mkdir -p /opt/docker/tmp
 
 
-mkdir -p /opt/docker/run/portainer
 
+
+mkdir -p /opt/docker/run/nginx
+mkdir -p /opt/docker/run/portainer
 
 # Set correct permissions for the directories and configuration file
 
@@ -46,35 +39,12 @@ chmod 755 /opt/docker/run
 chmod 755 /opt/docker/tmp
 chmod 755 /opt/docker/build
 
-mkdir -p /opt/docker/run/portainer
-mkdir -p /opt/docker/run/nginx
 
-
-
-# Set the timezone as a variable (change this as needed)
-TIMEZONE="Australia/Adelaide"
-
-# Create nginx portainer config file
-sudo tee /opt/docker/run/nginx/portainer.conf > /dev/null <<EOF
-
-
-server {
-  listen        80;
-  server_name   portainer.axfr.cc;
-
-  location / {
-    proxy_pass  http://portainer:9000;
-  }
-}
-
-EOF
 
 
 # Create the docker-compose.yml file
 cat > /opt/docker/build/docker-compose.yml <<EOF
 version: "3"
-networks:
-  net_portainer:
 volumes:
   vol_portainer:
     driver: local
@@ -89,45 +59,7 @@ volumes:
       device: /opt/docker/run/nginx
       o: bind
 services:
- nginx-base:
-    image: nginx:latest
-    environment:
-      - PUID=1000
-      - PGID=1000
-      - TZ=$TIMEZONE
-      - UMASK_SET=022
-    volumes:
-      - /etc/localtime:/etc/localtime:ro
-      - /var/run/docker.sock:/var/run/docker.sock:ro
-      - /etc/timezone:/etc/timezone:ro
-      - vol_nginx:/etc/nginx/conf.d/:ro
-    ports:
-      - 80:80
-      - 443:443
-    networks:
-      net_portainer:
-    labels:
-      - com.centurylinklabs.watchtower.enable=true
- portainer:
-    image: portainer/portainer-ce:latest
-    container_name: portainer
-    restart: always
-    security_opt:
-      - no-new-privileges:true
-    volumes:
-      - /etc/localtime:/etc/localtime:ro
-      - /var/run/docker.sock:/var/run/docker.sock:ro
-      - vol_portainer:/data
-    environment:
-      - PUID=1000
-      - PGID=1000
-      - TZ=$TIMEZONE
-      - UMASK_SET=022 #optional
-    networks:
-      net_portainer:
-    labels:
-      - "com.centurylinklabs.watchtower.enable=true"
- watchtower:
+  watchtower:
     image: containrrr/watchtower
     restart: always
     volumes:
@@ -144,9 +76,58 @@ services:
       - WATCHTOWER_INCLUDE_RESTARTING=true
     labels:
       - "com.centurylinklabs.watchtower.enable=true"
-
+  portainer:
+    container_name: portainer
+    image: portainer/portainer-ce:latest
+    restart: always
+    security_opt:
+      - no-new-privileges:true
+    volumes:
+      - /etc/localtime:/etc/localtime:ro
+      - /var/run/docker.sock:/var/run/docker.sock:ro
+      - vol_portainer:/data
+    environment:
+      - PUID=1000
+      - PGID=1000
+      - TZ=$TIMEZONE
+      - UMASK_SET=022 #optional
+    ports:
+     - 9443:9443
+    labels:
+      - "com.centurylinklabs.watchtower.enable=true"
+  app:
+    image: 'jc21/nginx-proxy-manager:latest'
+    environment:
+      - PUID=1000
+      - PGID=1000
+      - TZ=$TIMEZONE
+      - UMASK_SET=022 #optional
+    restart: always
+    ports:
+      - '80:80'
+      - '81:81'
+      - '443:443'
+    volumes:
+      - vol_nginx:/data
+    labels:
+      - com.centurylinklabs.watchtower.enable=true
+  portainer_agent:
+    image: portainer/agent
+    environment:
+      - PUID=1000
+      - PGID=1000
+      - TZ=$TIMEZONE
+      - UMASK_SET=022 #optional
+    ports:
+      - '9001:9001'
+    volumes:
+      - /etc/localtime:/etc/localtime:ro
+      - /var/run/docker.sock:/var/run/docker.sock:ro
+    labels:
+      - com.centurylinklabs.watchtower.enable=true
 EOF
 
 # Run docker-compose up against the docker-compose.yml file
 cd /opt/docker/build
 docker compose up -d
+
